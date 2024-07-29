4096e41c2b16b573719483764f6ce4e7097d6332
Lalu <Lalu@DESKTOP-VA7SI4H>
2024-07-29 13:02:56 +0530

    commit code

---
+++ SpringBootEx-1/src/main/java/com/ex/SpringBootEx1Application.java
@@ -0,0 +1,14 @@
+package com.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEx1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEx1Application.class, args);
+		System.out.println("Application has been started!");
+	}
+
+}
---
+++ SpringBootEx-1/src/main/java/com/ex/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.ex.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/names")
+	public String getNames()
+	{
+		return "Hello";
+	}
+	
+}
